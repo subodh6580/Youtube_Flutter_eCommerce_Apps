0f6f818736452fa3f39a29e1868e56d738cbe691
subodh6580 <subodh6580@gmail.com>
2024-04-14 23:23:58 +0800

    Login&Signup Part2 added

---
+++ Live_Class_12_shopping_hub_Login&Signup_Part2/lib/bottom_navigationbar.dart
@@ -0,0 +1,69 @@
+import 'package:flutter/material.dart';
+import 'package:shopping_hub/chat_page.dart';
+import 'package:shopping_hub/homepage.dart';
+import 'package:shopping_hub/shopping_cart.dart';
+import 'package:shopping_hub/user_profile.dart';
+
+class BottomNavigationBarPage extends StatefulWidget {
+  const BottomNavigationBarPage({super.key});
+
+  @override
+  State<BottomNavigationBarPage> createState() =>
+      _BottomNavigationBarPageState();
+}
+
+class _BottomNavigationBarPageState extends State<BottomNavigationBarPage> {
+  int _selectedIndex = 0;
+  static const TextStyle optionStyle =
+      TextStyle(fontSize: 30, fontWeight: FontWeight.bold);
+
+  List<Widget> widgetOptions = <Widget>[
+    //INDEX = HOMEPAGE
+    HomeScreenPage(),
+    //INDEX = CHAT PAGE
+    ChatScreenPage(),
+    //INDEX = CART PAGE
+    ShoppingCartPage(),
+    //INDEX = 3 PROFILE PAGE
+    UserProfilePage(),
+  ];
+
+  void _onItemTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: widgetOptions.elementAt(_selectedIndex),
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        items: const <BottomNavigationBarItem>[
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home),
+            label: 'Home',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.chat),
+            label: 'Chat',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.shopping_cart),
+            label: 'Cart',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.person),
+            label: 'Profile',
+          ),
+        ],
+        currentIndex: _selectedIndex,
+        selectedItemColor: Colors.amber[800],
+        unselectedItemColor: Colors.grey,
+        onTap: _onItemTapped,
+      ),
+    );
+  }
+}
